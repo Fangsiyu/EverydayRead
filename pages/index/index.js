719cc87354f939fe6ee4538a1f45f8ab08212ca7
fangsiyu <onlyfangsiyu@163.com>
2017-04-07 22:41:09 +0800

    提交首页和文章列表页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,26 +1,13 @@
 //index.js
-//获取应用实例
-var app = getApp()
 Page({
   data: {
-    motto: '你好！',
-    userInfo: {}
+    message: '「让阅读成为一种习惯」',
+    btnstart:'讀',
+    imgurl:'../imgs/logo.png'
   },
-  //事件处理函数
-  bindViewTap: function() {
+  toList:function(event){
     wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
+      url:'../article_list/article_list'
     })
   }
 })
