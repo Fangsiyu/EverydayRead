f004528f712c97edd1e19009dfab197562007c7e
fangsiyu <431290494>
2018-01-25 18:40:31 +0800

    跟新授权

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
 //app.js
 App({
     onLaunch: function () {
-        //登录
-        wx.login({
-            success: function (res) {
-                //获取用户信息
-                wx.getUserInfo({
-                    success: function (res) {
-                        wx.setStorageSync('dayUser', res.userInfo);
-                    }
-                });
-
-                console.log(wx.getStorageSync('dayUser'));
-            }
-        });
+        this.isGetUserMes();
     },
     //全局变量
     globalData: {
-        userInfo: "你是谁？",
+        userName:'天天阅读',
+        userImg:'/pages/imgs/user.png',
         proServer: "http://everydayread.gproom.com",//正式环境地址
         testServer: "",//测试环境地址
         list: [{
@@ -63,5 +52,52 @@ App({
             content: '777早上9点半，证券经纪人哈维·马克斯韦尔在年轻女速记员的陪同下，步履轻快地来到办公室...'
         }
         ]
-    }
+    },
+    //验证用户信息授权
+    isGetUserMes: function () {
+        //获取授权信息
+        wx.getUserInfo({            
+            success: res => {
+                console.log("初次授权成功");
+                wx.setStorage({  //用户信息存入缓存
+                    key: 'dayUser',
+                    data: res.userInfo,
+                });
+                console.log("初次用户信息存入缓存成功");
+                wx.getStorage({
+                    key: 'dayUser',
+                    success: function (res) {                        
+                        console.log(res.data.nickName)
+                    },
+                });
+            },
+            fail: res => {
+                console.log("初次授权失败");
+                wx.showModal({
+                    title: '友情提醒',
+                    content: '请先授权，以展示头像信息等',
+                    showCancel: false,
+                    confirmText: '好的',
+                    success: res => {
+                        wx.openSetting({
+                            success: res => {
+                                console.log(res);
+                                if (res.authSetting['scope.userInfo']) { //授权成功
+                                    console.log("二次授权成功");
+                                    wx.setStorage({  //用户信息存入缓存
+                                        key: 'dayUser',
+                                        data: res.userInfo,
+                                    });
+                                } else {
+                                    wx.clearStorage();
+                                    console.log("clean");
+                                    console.log("二次授权失败，清理缓存");
+                                }
+                            }
+                        })
+                    }
+                })
+            }
+        })
+    },
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,39 +1,42 @@
 //index.js
 var app = getApp();
 Page({
-  data: {
-    message: '「让阅读成为一种习惯」',
-    btnstart: '讀',
-    userImg: '../imgs/user.png',
-    userName: app.globalData,
-    imgurl: '../imgs/logo.png'
-  },
-  onShareAppMessage: function () {
-    // wx.showShareMenu();
-    return {
-      title: '每日阅读One',
-      path: '/pages/index/index',
-      success: function (res) {
-      }
-    }
-  },
-  onLoad: function (options) {
-    // 页面初始化 options为页面跳转所带来的参数
-    wx.showLoading({
-      title: '加载中...',
-      icon: 'loading'
-    });
-  },
-  onReady: function () {
-    wx.hideLoading()
-    // 页面渲染完成
-  },
-  toList: function (event) {
-    wx.navigateTo({
-      url: '../article_list/article_list'
-    })
-  },
-  onPullDownRefresh: function(){
-    wx.stopPullDownRefresh()
-  },  
+    data: {
+        message: '「让阅读成为一种习惯」',
+        btnstart: '讀',
+        userImg: app.globalData.userImg,
+        userName: app.globalDatauserName
+    },
+    onShareAppMessage: function () {
+        // wx.showShareMenu();
+        return {
+            title: '每日阅读One',
+            path: '/pages/index/index',
+        }
+    },
+    onLoad: function (options) {
+        // app.isGetUserMes();
+        // 页面初始化 options为页面跳转所带来的参数
+        wx.showLoading({
+            title: '加载中...',
+            icon: 'loading'
+        });
+        var dayUser = wx.getStorageSync('dayUser');
+        this.setData({
+            userImg: dayUser.avatarUrl,
+            userName: dayUser.nickName,
+        });
+    },
+    onReady: function () {
+        wx.hideLoading()
+        // 页面渲染完成
+    },
+    toList: function (event) {
+        wx.navigateTo({
+            url: '../article_list/article_list'
+        })
+    },
+    onPullDownRefresh: function () {
+        wx.stopPullDownRefresh()
+    },
 })
